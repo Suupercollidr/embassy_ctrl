e73799ec6383011a768af0c2ef4e258a5e7c28b9
Suupercollidr <139781600+Suupercollidr@users.noreply.github.com>
2026-06-30 22:15:00 +0200

    Improved button logic, improved reconnection for WiFi and MQTT, always log camera state changes and button pushes, renamed camera button "privacy button", removed troubleshooting code: nvs state, influx connection test, MQTT TCP test

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,8 @@
 #include <InfluxDbCloud.h>
 #include "debounce.h"
 #include "EventLogger.h"
-#include "nvsDebugData.h"
-#include "configuration.h"
-// #include "dev_configuration.h"
+// #include "configuration.h"
+#include "dev_configuration.h"
 
 WebServer localWebServer(80);
 AsyncMqttClient mqttClient;
@@ -20,11 +19,11 @@ InfluxDBClient influxLogClient(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_LOG_BUCKET, 
 
 EventLogger eventLog(influxLogClient, -1);
 
-Debounce cameraButtonDebounce;
+Debounce WiFiConnectTimeout(30000); // Used for both intial connect and reconnect period
+Debounce MQTTReconnect(10000);
+Debounce privacyButtonDebounce;
 Debounce inverterPowerChangeInterval(INV_RETRY_PERIOD * 1000);
 Debounce NTPSyncInterval(NTP_SYNC_INTERVAL * 3600000);
-Debounce MQTTReconnect(10000);
-
 
 enum powerSwitch
 {
@@ -34,12 +33,13 @@ enum powerSwitch
 
 powerSwitch inverterPowerState = ON;
 powerSwitch xmasLightState = OFF;
-volatile powerSwitch cameraTarget = ON;
 powerSwitch cameraState = ON;
-
-const char *getResetReason(esp_reset_reason_t);
+volatile powerSwitch cameraTarget = ON;
+volatile bool privacyButtonPushed;
 
 void initWiFi();
+void reconnectWiFi();
+void reconnectMqtt();
 void onMqttConnect(bool sessionPresent);
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason);
 void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties,
@@ -47,16 +47,13 @@ void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties 
 void controlInverter();
 void controlLight();
 void controlCamera();
-void IRAM_ATTR camButtonPush();
+void IRAM_ATTR privacyButtonPush();
+void privacyButtonAction();
 
 void setup()
 {
-
-  initNvs();
-  String lastEventBeforeReboot = String(readDataFromNvs("lastState"));
-  esp_reset_reason_t resetReason = esp_reset_reason();
-
   Serial.begin(115200);
+  delay(1000);
   Serial.println("====================");
   Serial.println(" System is starting ");
   Serial.println("====================");
@@ -65,31 +62,19 @@ void setup()
   pinMode(RELAY_LIGHT, OUTPUT);
   pinMode(RELAY_CAM, OUTPUT);
   pinMode(RELAY_AUX, OUTPUT);
-  pinMode(LED_CAM_BUTTON, OUTPUT);
-  pinMode(CAM_BUTTON, INPUT_PULLUP);
+  pinMode(LED_PRIVACY_BUTTON, OUTPUT);
+  pinMode(PRIVACY_BUTTON, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(CAM_BUTTON), camButtonPush, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PRIVACY_BUTTON), privacyButtonPush, FALLING);
 
   // Make sure relay positions match the corresponding power switch
   digitalWrite(RELAY_INV, HIGH);                                  // NC
   digitalWrite(RELAY_LIGHT, (xmasLightState == ON) ? HIGH : LOW); // NO
   digitalWrite(RELAY_CAM, (cameraState == ON) ? LOW : HIGH);      // NC
   digitalWrite(RELAY_AUX, LOW);
-  digitalWrite(LED_CAM_BUTTON, LOW);
+  digitalWrite(LED_PRIVACY_BUTTON, LOW);
 
   initWiFi();
-
-  // Testa om MQTT-brokers IP är nåbar via TCP
-  WiFiClient testClient;
-  if (testClient.connect(MQTT_HOST, 1883))
-  {
-    eventLog.log("TCP-anslutning till MQTT-broker lyckades", EventLogger::LogLevel::INFO);
-    testClient.stop();
-  }
-  else
-  {
-    eventLog.log("TCP-anslutning till MQTT-broker MISSLYCKADES", EventLogger::LogLevel::ERROR);
-  }
 
   timeSync(TIME_ZONE, NTP_SERVER1, NTP_SERVER2, NTP_SERVER3);
 
@@ -108,37 +93,23 @@ void setup()
   mqttClient.setServer(MQTT_HOST, 1883);
   mqttClient.connect();
 
-  if (!influxLogClient.validateConnection())
-    eventLog.log(String("Kunde inte ansluta till Influx DB på " + influxLogClient.getServerUrl() + "\nFelmeddelande:\n" + influxLogClient.getLastErrorMessage() + "\n"), EventLogger::LogLevel::ERROR);
   float setupTime = millis() / 1000.0f;
-
   eventLog.log(String("Systemet startat. Uppstarten tog " + String(setupTime) + " s."), EventLogger::LogLevel::INFO);
-  eventLog.log(String("Senaste återställning: " + String(getResetReason(resetReason)) + " (" + String(resetReason) + ")"), EventLogger::LogLevel::INFO);
-  eventLog.log(String("Senaste åtgärd: " + lastEventBeforeReboot), EventLogger::LogLevel::INFO);
-
-  storeDataToNvs("lastState", "Setup end");
 }
 
 void loop()
 {
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    WiFi.reconnect();
-    eventLog.log("Återansluter till WiFi", EventLogger::LogLevel::INFO);
-    delay(500);
+  if (!WiFi.isConnected())
+    reconnectWiFi();
 
-    if (WiFi.status() == WL_CONNECTED && !mqttClient.connected())
-      mqttClient.connect();
-  }
-
-  if (MQTTReconnect.ready())
-  {
-    eventLog.log("Försöker återansluta till MQTT...", EventLogger::LogLevel::INFO);
-    mqttClient.connect();
-  }
+  if (!mqttClient.connected())
+    reconnectMqtt();
 
   localWebServer.handleClient();
   ElegantOTA.loop();
+
+  if (privacyButtonPushed)
+    privacyButtonAction();
 
   controlCamera();
 
@@ -150,16 +121,21 @@ void loop()
 
 void initWiFi() // Connect to WiFi
 {
-  storeDataToNvs("lastState", "initWiFi");
   eventLog.log(String("Ansluter till WiFi " + String(ssid)), EventLogger::LogLevel::INFO);
   eventLog.log(String("MAC-adress: " + WiFi.macAddress()), EventLogger::LogLevel::INFO);
 
+  WiFi.setHostname(hostname);
   WiFi.begin(ssid, password, 6);
-  while (WiFi.status() != WL_CONNECTED)
+  while (!WiFi.isConnected())
   {
+    if (WiFiConnectTimeout.ready())
+    {
+      Serial.println("💥  ");
+      eventLog.log("Kunde inte ansluta till WiFi, startar om", EventLogger::LogLevel::ERROR);
+      ESP.restart();
+    }
     delay(100);
-
-    Serial.print(".");
+    Serial.print("🛜  ");
   }
   IPAddress myIp = WiFi.localIP();
   const String myIpString = String(myIp[0]) + "." +
@@ -180,15 +156,35 @@ void initWiFi() // Connect to WiFi
   Serial.println("\t\t\t\t\tIP-adress\t" + myIpString);
   Serial.println("\t\t\t\t\tGateway  \t" + gwIpString);
 
-  /*Point netStat("Network");
+  Point netStat("Network");
   netStat.addField("Channel", WiFi.channel());
   netStat.addField("Hostname", WiFi.getHostname());
   netStat.addField("IP address", myIpString);
   netStat.addField("Gateway", gwIpString);
   netStat.addField("MAC address", WiFi.macAddress());
 
-  influxClient.writePoint(netStat);
-  */
+  influxLogClient.writePoint(netStat);
+}
+
+void reconnectWiFi()
+{
+  if (!WiFiConnectTimeout.ready())
+    return;
+
+  eventLog.log("Återansluter till WiFi", EventLogger::LogLevel::INFO);
+  WiFi.reconnect();
+}
+
+void reconnectMqtt()
+{
+  if (!MQTTReconnect.ready())
+    return;
+
+  if (!WiFi.isConnected()) // Need WiFi to connect to MQTT broker
+    return;
+
+  eventLog.log("Försöker återansluta till MQTT...", EventLogger::LogLevel::INFO);
+  mqttClient.connect();
 }
 
 void onMqttConnect(bool sessionPresent)
@@ -205,6 +201,9 @@ void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
   String message = "Frånkopplad från MQTT-broker p.g.a.: ";
   message += static_cast<int>(reason);
   eventLog.log(message, EventLogger::LogLevel::WARNING);
+
+  if (reason != AsyncMqttClientDisconnectReason::MQTT_NOT_AUTHORIZED)
+    reconnectMqtt();
 }
 
 void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties,
@@ -220,12 +219,12 @@ void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties 
   if (message == "ON")
   {
     cameraTarget = ON;
-    eventLog.log("MQTT: Kamera ON-kommando mottaget", EventLogger::LogLevel::INFO);
+    eventLog.log("MQTT: Kamera ON-kommando mottaget", EventLogger::LogLevel::INFO, true);
   }
   else if (message == "OFF")
   {
     cameraTarget = OFF;
-    eventLog.log("MQTT: Kamera OFF-kommando mottaget", EventLogger::LogLevel::INFO);
+    eventLog.log("MQTT: Kamera OFF-kommando mottaget", EventLogger::LogLevel::INFO, true);
   }
 }
 
@@ -233,43 +232,43 @@ void controlInverter()
 {
   if (!inverterPowerChangeInterval.ready())
     return;
-/*
-const auto &intData = mpptData.getIntMap(); // Battery voltage in mV. Using MPPT voltage, since Inv. voltage = 0 when off
+  /*
+  const auto &intData = mpptData.getIntMap(); // Battery voltage in mV. Using MPPT voltage, since Inv. voltage = 0 when off
 
-auto it = intData.find("V");
-if (it == intData.end())
-{
-  eventLog.log("Hittade ingen batterispänning från MPPT", EventLogger::LogLevel::WARNING);
-  return;
-}
-const int voltage = it->second;
+  auto it = intData.find("V");
+  if (it == intData.end())
+  {
+    eventLog.log("Hittade ingen batterispänning från MPPT", EventLogger::LogLevel::WARNING);
+    return;
+  }
+  const int voltage = it->second;
 
-// Do nothing on coco-bananas values (<1 V or >20 V)
-if (voltage < 1000 || voltage > 20000)
-{
-  String messageText = "Orealistikt spänningsvärde (" + String(voltage) + " mV), ändrar inte status på inverter";
-  eventLog.log(messageText, EventLogger::LogLevel::WARNING);
-  return;
-}
+  // Do nothing on coco-bananas values (<1 V or >20 V)
+  if (voltage < 1000 || voltage > 20000)
+  {
+    String messageText = "Orealistikt spänningsvärde (" + String(voltage) + " mV), ändrar inte status på inverter";
+    eventLog.log(messageText, EventLogger::LogLevel::WARNING);
+    return;
+  }
 
-// If battery voltage is lower than off voltage, turn inverter off
-if (inverterPowerState == ON && voltage < INV_OFF_VOLTAGE)
-{
-  digitalWrite(RELAY_INV, HIGH); // Relay is NC, so triggering it will turn off the inverter
-  inverterPowerState = OFF;
-  eventLog.log("Inverter stängdes av, låg batterispänning", EventLogger::LogLevel::INFO);
-  return;
-}
+  // If battery voltage is lower than off voltage, turn inverter off
+  if (inverterPowerState == ON && voltage < INV_OFF_VOLTAGE)
+  {
+    digitalWrite(RELAY_INV, HIGH); // Relay is NC, so triggering it will turn off the inverter
+    inverterPowerState = OFF;
+    eventLog.log("Inverter stängdes av, låg batterispänning", EventLogger::LogLevel::INFO);
+    return;
+  }
 
-// If battery voltage is higher than on voltage, turn inverter on
-if (inverterPowerState == OFF && voltage > INV_ON_VOLTAGE)
-{
-  digitalWrite(RELAY_INV, LOW); // Relay is NC, so releasing it will turn on the inverter
-  inverterPowerState = ON;
-  eventLog.log("Inverter slogs på, tillräcklig batterispänning ", EventLogger::LogLevel::INFO);
-  return;
-}
-*/
+  // If battery voltage is higher than on voltage, turn inverter on
+  if (inverterPowerState == OFF && voltage > INV_ON_VOLTAGE)
+  {
+    digitalWrite(RELAY_INV, LOW); // Relay is NC, so releasing it will turn on the inverter
+    inverterPowerState = ON;
+    eventLog.log("Inverter slogs på, tillräcklig batterispänning ", EventLogger::LogLevel::INFO);
+    return;
+  }
+  */
 }
 
 /**
@@ -282,25 +281,25 @@ void controlLight()
 {
   /*
   const auto &intData = mpptData.getIntMap(); // Contains panel voltage (VPV) in mV
-  
+
   auto it = intData.find("VPV");
   if (it == intData.end())
   {
     eventLog.log("Hittade ingen panelspänning från MPPT", EventLogger::LogLevel::WARNING);
     return;
   }
-  
+
   const int panelVoltage = it->second;
-  
+
   time(&now);
   struct tm *timeinfo = localtime(&now);
-  
+
   const bool isXmas = (timeinfo->tm_mon == 11 && timeinfo->tm_mday >= 1) || (timeinfo->tm_mon == 0 && timeinfo->tm_mday <= 13);
   const bool isDark = (panelVoltage < 5000);
   const bool isDay = (timeinfo->tm_hour >= 8) && (timeinfo->tm_hour < 20);
-  
+
   xmasLightState = (isXmas && isDark && isDay) ? ON : OFF;
-  
+
   xmasLightState ? digitalWrite(RELAY_LIGHT, HIGH) : digitalWrite(RELAY_LIGHT, LOW);
   */
 }
@@ -318,17 +317,17 @@ void controlCamera()
   switch (cameraState)
   {
   case ON:
-    digitalWrite(LED_CAM_BUTTON, LOW); // Green light off when cam on
-    digitalWrite(RELAY_CAM, LOW);      // Relay is NC
+    digitalWrite(LED_PRIVACY_BUTTON, LOW); // Green light off when cam on
+    digitalWrite(RELAY_CAM, LOW);          // Relay is NC
     mqttClient.publish(camera_state_topic, 1, true, "ON");
-    eventLog.log("Camera turned on", EventLogger::LogLevel::INFO);
+    eventLog.log("Camera turned on", EventLogger::LogLevel::INFO, true);
     break;
 
   case OFF:
-    digitalWrite(LED_CAM_BUTTON, HIGH); // Green light indicates cam is OFF
-    digitalWrite(RELAY_CAM, HIGH);      // Relay is NC
+    digitalWrite(LED_PRIVACY_BUTTON, HIGH); // Green light indicates cam is OFF
+    digitalWrite(RELAY_CAM, HIGH);          // Relay is NC
     mqttClient.publish(camera_state_topic, 1, true, "OFF");
-    eventLog.log("Camera turned off", EventLogger::LogLevel::INFO);
+    eventLog.log("Camera turned off", EventLogger::LogLevel::INFO, true);
     break;
 
   default:
@@ -338,8 +337,15 @@ void controlCamera()
   }
 }
 
-void IRAM_ATTR camButtonPush()
+void IRAM_ATTR privacyButtonPush()
 {
-  if (cameraButtonDebounce.ready())
-    cameraTarget = (cameraTarget == ON) ? OFF : ON;
+  if (privacyButtonDebounce.ready())
+    privacyButtonPushed = true;
+}
+
+void privacyButtonAction()
+{
+  privacyButtonPushed = false;
+  cameraTarget = (cameraTarget == ON) ? OFF : ON;
+  eventLog.log("Privacy button pushed", EventLogger::LogLevel::INFO, true);
 }
